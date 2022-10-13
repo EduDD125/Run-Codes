990d4a9435322e347b162bc5d0166bde8e5e5c86
EduDD125 <115571853+EduDD125@users.noreply.github.com>
2022-10-13 12:52:12 -0300

    Add files via upload
    Problema;    Sejam A, B e C os três lados de um triângulo. Faça um programa que leia o valor de três lados de um triângulo A, B e C. Seu algoritmo deve informar se o triangulo é: Escaleno (todos os lados diferentes); Isósceles (possui dois lados iguais); e Equilátero (todos os lados iguais); Não forma triângulo (quando a soma de dois lados é menor que o terceiro lado).

---
+++ C010.py
@@ -0,0 +1,20 @@
+a = int(input())
+b = int(input())
+resto=1
+
+if a>b:
+    while resto > 0:
+        resto = a%b
+        a=b
+        b=resto
+    mdc=a
+elif a<b:
+    while resto > 0:
+        resto= b%a
+        b=a
+        a=resto
+    mdc=b
+else:
+    mdc=a
+
+print(mdc)
