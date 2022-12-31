9819fe8f93556f22e2e8855a73ef8e06aaff5630
EduDD125 <115571853+EduDD125@users.noreply.github.com>
2022-12-31 17:55:02 -0300

    Update B006.py

--- B006.py
+++ B006.py
@@ -1,13 +1,15 @@
-a=float(input())
-b=float(input())
-c=float(input())
+# Programa que lê os coeficientes a, b e c de uma equação do segundo grau ax² + bx + c, calcule e imprima a soma das raízes da equação. Para valor não reais o resultado é 'nan'. 
 
-delta=(b**2)-(4*a*c)
+a = float(input())
+b = float(input())
+c = float(input())
 
-if delta>=0:
-    x1=(-b+(delta**(1/2)))/(2*a)
-    x2=(-b-(delta**(1/2)))/(2*a)
-    soma=x1+x2
+delta = (b**2)-(4*a*c)
+
+if delta >= 0:
+    x1 = (-b + (delta**(1/2))) / (2*a)
+    x2 = (-b - (delta**(1/2))) / (2*a)
+    soma = x1 + x2
     print(f"{soma:.2f}")
 else:
     import math
