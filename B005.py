927ea7eb9aba199ffe747880cf0852d92ab08385
EduDD125 <115571853+EduDD125@users.noreply.github.com>
2022-12-31 17:52:06 -0300

    Update B005.py

--- B005.py
+++ B005.py
@@ -1,12 +1,14 @@
-a=int(input())
+# Determinar se o ano é bissexto. verdade = 1; falso = 0 e entrada invalidade = 0
+
+ano = int(input())
 
 
-if a<=0:
+if ano <= 0:
     print(-1)
-elif a%400==0:
+elif ano%400==0:
     print(1)
-elif a%4==0:
-    if a%100==0:
+elif ano%4==0:
+    if ano%100==0:
         print(0)
     else:
         print(1)
