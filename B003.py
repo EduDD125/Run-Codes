3fc761708d41ec887e079728194233d6606df399
EduDD125 <115571853+EduDD125@users.noreply.github.com>
2022-12-31 17:40:22 -0300

    Update B003.py

--- B003.py
+++ B003.py
@@ -1,10 +1,12 @@
-i=int(input())
+# Terminar o comprimisso eleitoral do cidadão a partir de sua idade
 
-if i>69:
+idade = int(input())
+
+if idade > 69:
     print("DISPENSADO")
-elif i>=18:
-    print("OBRIGATORIO")
-elif 16<=i and i<=17:
-    print("FACULTATIVO")
+elif idade >= 18:
+    print("VOTO OBRIGATORIO")
+elif idade >= 16 and idade <= 17:
+    print("VOTO FACULTATIVO")
 else:
     print("DISPENSADO")
