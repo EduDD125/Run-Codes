688deb06a55dcec7017cb2a22d141aab0682d2bf
EduDD125 <115571853+EduDD125@users.noreply.github.com>
2022-12-31 17:32:28 -0300

    Update B002.py

--- B002.py
+++ B002.py
@@ -1,12 +1,12 @@
-l=int(input())
-m=float(input())
-a=float(input())
-v=int(input())
+velocidade_max = int(input())
+multa_min = float(input())
+taxa = float(input())
+velocidade_carro=int(input())
 
 multa=m+a*(v-l)
 
-if v>l:
-    multa=m+a*(v-l)
+if velocidade_carro > velocidade_max:
+    multa = multa_min + taxa * (velocidade_carro - velocidade_max)
     print(f"{multa:.2f}")
 else:
     multa=0
